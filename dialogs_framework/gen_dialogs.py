e5d8d21674e5a4676ece25fa5472979d58cd4952
gal kahana <gal.kahana@hotmail.com>
2022-01-27 17:10:42 +0200

    all scenarios tested, but that multi task thing

--- dialogs_framework/gen_dialogs.py
+++ dialogs_framework/gen_dialogs.py
@@ -1,71 +1,13 @@
 from itertools import count
 from typing import Optional, Union, cast
 
-from .types import send_message, get_client_response,DialogStepDone, ServerMessage, DialogStepNotDone, SendMessageFunction, SendToClientException, Dialog, VersionMismatchException
+from dialogs_framework.dialog_state import DialogState
+
+from .types import BaseDialog, send_message, get_client_response,DialogStepDone, ServerMessage, DialogStepNotDone, SendMessageFunction, SendToClientException, Dialog, VersionMismatchException
 from .message_queue import MessageQueue
 from .persistence.persistence import PersistenceProvider
 
 from .dialogs import T, ClientResponse, DialogContext, RunDialogReturnType, ServerResponse
-
-def _execute_step(step, state, send, client_response):
-    if isinstance(step, get_client_response):
-        if not state.sent_to_client:
-            state.sent_to_client = True
-            raise SendToClientException
-        else:
-            step_value = cast(T, client_response)
-    elif isinstance(step, send_message):
-        send(step.message)
-        step_value = None
-    elif isinstance(step, Dialog):
-        subdialog_context =  DialogContext(
-                state=state,
-                client_response=client_response,
-                send=send,
-                call_counter=count(),
-        )
-        # recurse and return as next value
-        step_value = _run(step, subdialog_context)
-    else:
-        raise Exception("Unsupported dialog type")
-
-    return step_value
-
-def _run(subdialog: Dialog[T], context: DialogContext) -> T:
-    state = context.state
-    # TODO: support non gens as well
-    instance = subdialog.dialog()
-
-    try:
-        value_for_next_step = None
-        while True:
-            next_step = instance.send(value_for_next_step)
-            subdialog_state = state.get_subdialog_state(next(context.call_counter), next_step)
-            if next_step.version != subdialog_state.version:
-                raise VersionMismatchException
-            if subdialog_state.is_done:
-                value_for_next_step = subdialog_state.return_value
-            else:
-                value_for_next_step = _execute_step(next_step, subdialog_state, context.send, context.client_response)
-                subdialog_state.return_value = value_for_next_step
-    except StopIteration as ex:
-        return ex.value
-
-# This is (initially at least) a copy of the existing dialogs run_fallback...only its run_dialog is run_gen_dialog
-# it should be possible to share most of the code, and pass through the dialog running function
-def _run_fallback_gen_dialog(client_response, dialog, persistence, fallback_dialog, state):
-    messages: ServerResponse = []
-    if fallback_dialog is not None:
-        next_step: RunDialogReturnType = run_gen_dialog(fallback_dialog, persistence, client_response)
-        if not next_step.is_done:
-            return next_step
-        messages = next_step.messages
-        # Fallback dialog completed
-        state.reset(dialog, fallback_mode=False)
-
-    next_step = run_gen_dialog(dialog, persistence, client_response, fallback_dialog)
-    next_step.messages = messages + next_step.messages
-    return next_step
 
 def run_gen_dialog(    
     dialog: Dialog[T],
@@ -86,10 +28,9 @@ def run_gen_dialog(
     if state.is_done:
         return state.return_value    
 
-    dialog_context = DialogContext(send=send, state=state, call_counter=count(), client_response=client_response)
     is_done = False
     try:
-        return_value = _run(dialog, dialog_context)
+        return_value = _run_base_dialog(dialog, state, send, client_response)
         is_done = True
     except VersionMismatchException:
         state.reset(dialog, fallback_mode=True)
@@ -103,3 +44,64 @@ def run_gen_dialog(
         return DialogStepDone(return_value=return_value, messages=messages)
     else:
         return DialogStepNotDone(messages=messages)
+
+def _run_base_dialog(base_dialog: BaseDialog[T], state: DialogState, send, client_response):
+    if isinstance(base_dialog, get_client_response):
+        if not state.sent_to_client:
+            state.sent_to_client = True
+            raise SendToClientException
+        else:
+            step_value = cast(T, client_response)
+    elif isinstance(base_dialog, send_message):
+        send(base_dialog.message)
+        step_value = None
+    elif isinstance(base_dialog, Dialog):
+        subdialog_context =  DialogContext(
+                state=state,
+                client_response=client_response,
+                send=send,
+                call_counter=count(),
+        )
+        # recurse and return as next value
+        step_value = _run_dialog(base_dialog, subdialog_context)
+    else:
+        raise Exception("Unsupported dialog type")
+
+    return step_value
+
+def _run_dialog(dialog: Dialog[T], context: DialogContext) -> T:
+    state = context.state
+    # TODO: support non gens as well
+    instance = dialog.dialog()
+
+    try:
+        value_for_next_step = None
+        while True:
+            next_step = instance.send(value_for_next_step)
+            next_step_state = state.get_subdialog_state(next(context.call_counter), next_step)
+            if next_step.version != next_step_state.version:
+                raise VersionMismatchException
+            if next_step_state.is_done:
+                value_for_next_step = next_step_state.return_value
+            else:
+                value_for_next_step = _run_base_dialog(next_step, next_step_state, context.send, context.client_response)
+                next_step_state.return_value = value_for_next_step
+    except StopIteration as ex:
+        return ex.value
+
+# This is (initially at least) a copy of the existing dialogs run_fallback...only its run_dialog is run_gen_dialog
+# it should be possible to share most of the code, and pass through the dialog running function
+def _run_fallback_gen_dialog(client_response, dialog, persistence, fallback_dialog, state):
+    messages: ServerResponse = []
+    if fallback_dialog is not None:
+        next_step: RunDialogReturnType = run_gen_dialog(fallback_dialog, persistence, client_response)
+        if not next_step.is_done:
+            return next_step
+        messages = next_step.messages
+        # Fallback dialog completed
+        state.reset(dialog, fallback_mode=False)
+
+    next_step = run_gen_dialog(dialog, persistence, client_response, fallback_dialog)
+    next_step.messages = messages + next_step.messages
+    return next_step
+
--- tests/gen/test_gen_dialogs.py
+++ tests/gen/test_gen_dialogs.py
@@ -32,6 +32,48 @@ def name_getter_dialog_take_2() -> str:
     result = yield get_client_response()
     return result
 
+@dialog(version="1.0")
+def fallback_with_client_response() -> None:
+    yield send_message("Falling back!")
+    yield get_client_response()
+    yield send_message("Get up fool")
+
+
+@dialog(version="1.0")
+def versioned_subdialog() -> str:
+    yield send_message("I am a dialog")
+    result = yield get_client_response()
+    return result
+
+
+@dialog(version="1.1")
+def versioned_subdialog_take_2() -> str:
+    yield send_message("I have a different version, HA! HA! HA!")
+    result = yield get_client_response()
+    return result
+
+@dialog(version="1.0")
+def dialog_with_subdialog() -> str:
+    yield versioned_subdialog()
+    result = yield get_client_response()
+    return result
+
+
+@dialog(version="1.0")
+def dialog_with_subdialog_take_2() -> str:
+    yield versioned_subdialog_take_2()
+    result = yield get_client_response()
+    return result
+
+
+@dialog(version="1.0")
+def name_getter_dialog_take_3() -> str:
+    yield send_message("I need to know your name")
+    name: str = yield get_client_response()
+    yield send_message("Wait! i have another message for you!")
+    return name
+
+
 def test_run_dialog_happy_flow():
     persistence = InMemoryPersistence()
 
@@ -82,3 +124,59 @@ def test_run_dialog_with_fallback_without_client_response():
     )
     assert step3.is_done
     assert step3.return_value == "Johnny"
+
+
+def test_run_dialog_with_fallback_with_client_response():
+    persistence = InMemoryPersistence()
+    step1 = run_gen_dialog(name_getter_dialog(), persistence, "", fallback_with_client_response())
+    assert len(step1.messages) == 3
+
+    step2 = run_gen_dialog(
+        name_getter_dialog_take_2(), persistence, "Juanito", fallback_with_client_response()
+    )
+    assert step2.messages == ["Falling back!"]
+    # We don't test for the return value of this part because we don't handle the internal fallback dialogs_framework return_value
+
+    step3 = run_gen_dialog(
+        name_getter_dialog_take_2(), persistence, "Julia", fallback_with_client_response()
+    )
+    assert step3.messages == ["Get up fool", "Tell me your name! Now!!!"]
+
+    step4 = run_gen_dialog(
+        name_getter_dialog_take_2(), persistence, "Johnny", fallback_with_client_response()
+    )
+    assert step4.is_done
+    assert step4.return_value == "Johnny"
+
+
+def test_run_dialog_with_fallback_on_subdialog_version_mismatch():
+    persistence = InMemoryPersistence()
+    step1 = run_gen_dialog(dialog_with_subdialog(), persistence, "", fallback_without_client_response())
+    assert step1.messages == ["I am a dialog"]
+
+    step2 = run_gen_dialog(
+        dialog_with_subdialog_take_2(), persistence, "Julia", fallback_without_client_response()
+    )
+    assert step2.messages == ["Falling back!", "I have a different version, HA! HA! HA!"]
+
+
+def test_run_dialog_with_fallback_truncates_leftover_messages():
+    persistence = InMemoryPersistence()
+    step1 = run_gen_dialog(
+        name_getter_dialog_take_3(), persistence, "", fallback_with_client_response()
+    )
+    assert step1.messages == ["I need to know your name"]
+
+    step2 = run_gen_dialog(
+        name_getter_dialog_take_2(), persistence, "Julia", fallback_without_client_response()
+    )
+    # We expect the second message of `name_getter_dialog_take_3` not to be here
+    assert step2.messages == ["Falling back!", "Tell me your name! Now!!!"]
+
+
+def test_run_dialog_returns_leftover_messages_when_done():
+    persistence = InMemoryPersistence()
+    next_step = run_gen_dialog(send_message("what is your name?"), persistence, "")
+
+    assert next_step.is_done
+    assert next_step.messages == ["what is your name?"]
